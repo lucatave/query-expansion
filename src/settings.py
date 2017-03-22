7289d1cf918108e4ef7a6856dd7fba45fef4afa2
Luca Tavernari <tehluke94@gmail.com>
2017-03-22 19:21:50 +0100

    Fix settings

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
