95989bc869eeb3523d3d77dc8c21e9bd85224591
Luca Tavernari <tehluke94@gmail.com>
2017-03-28 01:59:02 +0200

    Add download and save feature

--- src/data.py
+++ src/data.py
@@ -1,9 +1,53 @@
 from os import environ
 from twitter import Api
+from peewee import OperationalError
+from models import (User, Document, Annotation, Tweet,
+                    AnnotationSimilarity, db)
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
 
 
-def save_data():
-    pass
+def print_info(tweet):
+    print(tweet.user.screen_name)
+    print(tweet.full_text or tweet.text)
+    print(tweet.hashtags)
+    print(tweet.contributors)
+    print(tweet.in_reply_to_screen_name)
+    print(tweet.user_mentions)
+    print("\n")
+
+
+def wait_for_db():
+    flag = True
+    while flag:
+        try:
+            db.connect()
+            flag = False
+        except OperationalError:
+            flag = True
+
+
+def create_db():
+    wait_for_db()
+    db.create_tables([User, Document, Annotation,
+                      Tweet, AnnotationSimilarity])
+
+
+def save_data(tweet, u_r, u_w):
+    with db.atomic():
+        User.get_or_create(id=u_w.id)
+        if Document.select(id).where(Document.id == tweet.id).count() == 0:
+            Document.get_or_create(id=tweet.id,
+                                   user_w=u_w.id,
+                                   user_r=u_r.id)
+        hashtags = {hashtag.text for hashtag in tweet.hashtags}
+        for hashtag in hashtags:
+            Annotation.get_or_create(id=hashtag)
+            Tweet.get_or_create(id_document=tweet.id,
+                                id_annotation=hashtag)
 
 
 def get_data():
@@ -18,6 +62,13 @@ def get_data():
               access_token_secret=user_secret)
 
     for friend_id in api.GetFriendIDs():
+        User.get_or_create(id=friend_id)
         friend_timeline = api.GetUserTimeline(user_id=friend_id, count=200)
+        friend = api.GetUser(user_id=friend_id)
+        print("GETTING STATUSES OF ", friend.screen_name)
         for tweet in friend_timeline:
-            save_data()
+            save_data(tweet, friend, tweet.user)
+        print("GETTING MENTIONS OF ", friend.screen_name)
+        for tweet in api.GetSearch(term=friend.screen_name, count=100):
+            if tweet.user.id != friend_id:
+                save_data(tweet, friend, tweet.user)
