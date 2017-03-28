d98810cd531c3a1871ba1ba93bd0a2d46e659f92
Luca Tavernari <tehluke94@gmail.com>
2017-03-28 02:01:14 +0200

    Fix models

--- src/models.py
+++ src/models.py
@@ -5,51 +5,53 @@ from peewee import (DoubleField, TextField,
 from playhouse.postgres_ext import PostgresqlDatabase
 from os import environ
 
+db = PostgresqlDatabase(environ["POSTGRES_DB"],
+                        user=environ["POSTGRES_USER"],
+                        password=environ["POSTGRES_PASSWORD"],
+                        host=environ["DB_HOST"],
+                        port=environ["DB_PORT"])
+
 
 class BaseModel(Model):
     class Meta:
-        database = PostgresqlDatabase(environ["POSTGRES_DB"],
-                                      user=environ["POSTGRES_USER"],
-                                      password=environ["POSTGRES_PASSWORD"],
-                                      host=environ["DB_HOST"],
-                                      port=environ["DB_PORT"])
+        database = db
 
 
 class User(BaseModel):
-    id_user = TextField(primary_key=True)
-    rank = DoubleField()
+    id = TextField(primary_key=True)
+    rank = DoubleField(null=True)
 
 
 class Document(BaseModel):
-    id_document = TextField(primary_key=True)
-    rank = DoubleField()
-
-
-class Annotation(BaseModel):
-    id_annotation = TextField(primary_key=True)
-    rank = DoubleField()
-
-
-class Tweet(BaseModel):
+    id = TextField(primary_key=True)
     user_w = ForeignKeyField(User, on_delete="CASCADE",
                              related_name="posted")
     user_r = ForeignKeyField(User, on_delete="CASCADE",
                              related_name="received")
+    rank = DoubleField(null=True)
+
+
+class Annotation(BaseModel):
+    id = TextField(primary_key=True)
+    rank = DoubleField(null=True)
+
+
+class Tweet(BaseModel):
     id_document = ForeignKeyField(Document, on_delete="CASCADE",
-                                  related_name="tweet")
-    annotation = ForeignKeyField(Annotation, on_delete="CASCADE",
-                                 related_name="tweets")
+                                  related_name="tweets")
+    id_annotation = ForeignKeyField(Annotation, on_delete="CASCADE",
+                                    related_name="tweets")
 
     class Meta:
-        indexes = (
-            (('user_w', 'user_r', 'id_document', 'annotation'), True)
-        )
+        primary_key = CompositeKey('id_document', 'id_annotation')
 
 
 class AnnotationSimilarity(BaseModel):
-    annotation1 = ForeignKeyField(Annotation, on_delete="CASCADE")
-    annotation2 = ForeignKeyField(Annotation, on_delete="CASCADE")
-    rank = DoubleField()
+    annotation1 = ForeignKeyField(Annotation, on_delete="CASCADE",
+                                  related_name="row")
+    annotation2 = ForeignKeyField(Annotation, on_delete="CASCADE",
+                                  related_name="column")
+    rank = DoubleField(null=True)
 
     class Meta:
         primary_key = CompositeKey('annotation1', 'annotation2')
